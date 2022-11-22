3c7f180502655eebcaf283ccb56a3743bddc8f06
Abdul Majid <abdulmajid1m2@gmail.com>
2022-11-22 15:36:39 +0500

    first commit

---
+++ app/Http/Controllers/pharmacyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\medicines;
+use App\Models\Cart;
+class pharmacyController extends Controller
+{
+    public function index(){
+        $medicines= medicines::all();
+        return view('welcome',['medicines'=>$medicines]);
+    }
+    
+    public function cart(Request $request,$medicineID){
+
+        if(Auth::id())
+        {
+            $user=auth()->user();
+            $medicine=medicines::find($medicineID);
+            $cart=new cart;
+            $cart->name=$user->name;
+            $cart->address=$user->address;
+            $cart->MedcineImage=$medicine->medicineImage;
+            $cart->MedcineName=$medicine->medicineName;
+            $cart->MedcinePrice=$medicine->medicinePrice;
+            $cart->Quantity=$request->Quantity;
+            $cart->save();
+
+            return redirect()->back();
+        }
+        else
+        {
+            return redirect('login');
+        }
+        // $medicines= medicines::all();
+        // return view('welcome',['medicines'=>$medicines]);
+    }
+}
+ 
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pharmacyController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [pharmacyController::class,'index']);
+
+Route::get('/cart', [pharmacyController::class,'cart']);
+
+Route::post('/cart/{medicineID}', [pharmacyController::class,'cart']);
+
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
